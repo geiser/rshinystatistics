b40cb5221f7e491e8850f787e6068ec0f95a247d
geiser <geiser.gcc@gmail.com>
2020-09-27 06:22:43 -0300

    fixing ancova plot

--- R/ancova_plots.R
+++ R/ancova_plots.R
@@ -4,18 +4,12 @@ ggPlotAoC <- function(data, y, x, color = c(), aov, pwc, linetype = color, font.
   pwc2 <- tryCatch(rstatix::add_xy_position(pwc, x=x, fun="mean_se", step.increase=step.increase), error = function(e) NULL)
 
   if (is.null(pwc2)) return(ggplot2::ggplot())
-  if (length(color) < 1) color <- "black"; linetype <- 1
 
   lp <- ggpubr::ggline(rstatix::get_emmeans(pwc2), size = 0.6, x=x, y="emmean", color=color, palette = "jco", plot_type='b')
-  lp <- lp + ggplot2::geom_jitter(data = data, ggplot2::aes_string(x=x,color=color,y=y),width=0.05,height=0.05,size=0.5)
+  lp <- lp + ggplot2::geom_jitter(data = data, ggplot2::aes_string(x=x,color=color,y=y),width=0.075,height=0.075,size=1)
+  lp <- lp + ggplot2::geom_errorbar(ggplot2::aes_string(ymin="conf.low", ymax="conf.high", color=color), width=0.1)
 
-  if (color != "black")
-    lp <- lp + ggplot2::geom_errorbar(ggplot2::aes_string(ymin="conf.low", ymax="conf.high", color=color), width=0.1)
-  else
-    lp <- lp + ggplot2::geom_errorbar(ggplot2::aes_string(ymin="conf.low", ymax="conf.high"), width=0.1)
-
-
-  lp <- lp + ggpubr::stat_pvalue_manual(pwc2, hide.ns=T, tip.length=0.005, bracket.size=0.3, linetype=linetype, color=color)
+  lp <- lp + ggpubr::stat_pvalue_manual(pwc2, hide.ns=T, tip.length=0.005, bracket.size=0.3, color=color)
   lp <- lp + ggplot2::labs(subtitle = rstatix::get_test_label(aov, detailed=T), caption=rstatix::get_pwc_label(pwc))
   lp <- lp + ggplot2::theme(text = ggplot2::element_text(size=font.label.size))
   return(lp)
@@ -39,7 +33,7 @@ oneWayAncovaPlots <- function(data, dv, ivs, aov, pwcs, font.label.size = 12, st
   return(lapply(livs, FUN = function(iv) {
     pwc <- pwcs[[iv]]
     color <- iv
-    lp <- ggPlotAoC(data, dv, iv, c(), aov, pwc, 1, font.label.size, step.increase)
+    lp <- ggPlotAoC(data, dv, iv, color, aov, pwc, color, font.label.size, step.increase)
     return(lp)
   }))
 }
