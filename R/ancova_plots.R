0a901698c3167f8d83dcc66c445fb0818a899088
Geiser C. Challco <geiser@alumni.usp.br>
2023-08-20 08:19:45 -0300

    adding boxplots

--- R/ancova_plots.R
+++ R/ancova_plots.R
@@ -325,3 +325,191 @@ twoWayAncovaBarPlots <- function(data, dv, ivs, aov, pwcs, covar=c(), pre.post =
   return(toReturn)
 }
 
+
+#' ANCOVA Box Plot
+#'
+#' This function create box plots to report results from ANCOVA (AoV).
+#'
+#' @param data a data.frame containing the data in which performing the ANOVA test
+#' @param dv a character string containing the name of variable used as x-axis
+#' @param iv a character string containing the name of variable used as y-axis
+#' @param aov the ANOVA statistical results returned by rstatix::anova_test
+#' @param pwc the statistical results returned by a pairwise comparisons between groups from rstatix
+#' @param covar a column name of covariance variable
+#' @param pre.post a column name of time variable (pre- and post-test phases)
+#' @param color a vector or character string containing the name of variable used as color in the box-plot
+#' @param theme the theme used in the box plot
+#' @param step.increase the numeric vector to be used to minimize the overlap
+#' @param subtitle the subtitle in the plot, use number to indicate the row from ANCOVA table
+#' @param ylim the number that indicates the axis-y limit
+#' @export
+ggBoxPlotAoC <- function (data, dv, iv, aov, pwc, covar = c(), pre.post = c(), color = c(),
+                          theme = c(), step.increase = 0.2, subtitle = c(), ylim = NA) {
+  bar.width = 0.75
+  if (is.null(aov) || is.null(pwc)) return(NULL)
+  if (is.null(covar) && is.null(pre.post)) return(NULL)
+
+  if (!is.null(covar)) {
+    df <- data.frame(variable = c(rep(covar, nrow(data)), rep(dv, nrow(data))))
+    df[[dv]] <- c(data[[covar]], data[[dv]])
+    df[[iv]] <- c(data[[iv]], data[[iv]])
+    df$variable <- factor(df$variable, c(covar, dv))
+
+    gg1 <- ggpubr::ggboxplot(df, "variable", dv, fill = iv, palette = color)
+
+    ngroup = length(unique((df[[iv]])))
+    xvars = levels(df$variable)
+    sig.g.comb = combn(unique(df[[iv]]), 2, simplify = F)
+    ycol = ".y."
+  }
+  else if (!is.null(pre.post)) {
+    df = data
+    if (!is.factor(df[[iv]]))
+      df[[iv]] <- factor(df[[iv]], unique(df[[iv]]))
+
+    gg1 <- ggpubr::ggboxplot(df, iv, dv, fill = pre.post, palette = color)
+
+    ngroup = length(unique((df[[pre.post]])))
+    xvars = levels(df[[iv]])
+    sig.g.comb = combn(unique(df[[pre.post]]), 2, simplify = F)
+    ycol = iv
+  }
+
+  if (!is.null(theme)) {
+    if (theme == "gray")
+      gg1 <- gg1 + ggplot2::theme_gray()
+    if (theme == "linedraw")
+      gg1 <- gg1 + ggplot2::theme_bw()
+    if (theme == "light")
+      gg1 <- gg1 + ggplot2::theme_light()
+    if (theme == "dark")
+      gg1 <- gg1 + ggplot2::theme_dark()
+    if (theme == "minimal")
+      gg1 <- gg1 + ggplot2::theme_minimal()
+    if (theme == "classic")
+      gg1 <- gg1 + ggplot2::theme_classic()
+    if (theme == "void")
+      gg1 <- gg1 + ggplot2::theme_void()
+  }
+
+  ylim1 = ylim
+  for (i in 1:length(xvars)) {
+    x = xvars[i]
+    sig.pre.y = max(df[[dv]], na.rm = T) + step.increase
+    x.space = bar.width/ngroup
+    sig.y = i - (bar.width/2) + (x.space/2)
+    sig.x.comb = combn(seq(sig.y, by = x.space, length.out = ngroup), 2, simplify = F)
+    for (j in 1:length(sig.x.comb)) {
+      x1 = sig.x.comb[[j]][1]; x2 = sig.x.comb[[j]][2]
+      g1 = sig.g.comb[[j]][1]; g2 = sig.g.comb[[j]][2]
+
+      p <- pwc$p.adj[pwc[[ycol]] == x & ((pwc$group1 == g1 & pwc$group2 == g2) |
+                                           (pwc$group1 == g2 & pwc$group2 == g1))]
+
+      if (!is.null(p) && !is.na(p) && length(p > 0) && p < 0.05) {
+        label = ifelse(p <= 0.01, ifelse(p <= 0.001, "***", "**"), "*")
+        gg1 <- gg1 + ggplot2::geom_segment(x = x1, y = sig.pre.y, xend = x2, yend = sig.pre.y) +
+          ggplot2::geom_text(x = (x1 + x2)/2, y = sig.pre.y + 0.02, label = label)
+        sig.pre.y = sig.pre.y + step.increase
+      }
+    }
+    if (is.na(ylim1) || ylim1 < sig.pre.y)
+      ylim1 = sig.pre.y
+  }
+  gg1 <- gg1 + ylim(NA, ylim1)
+  if (!is.null(subtitle) && is.numeric(subtitle)) {
+    if (subtitle == 0) {
+      row = which(aov$Effect == x)
+    }
+    else {
+      row = subtitle
+    }
+    subtitle = tryCatch(rstatix::get_test_label(aov, detailed = T, row = row), error = function(e) NULL)
+    if (is.null(subtitle)) {
+      p = ifelse(aov$p[row] < 0.01, "p < 0.01", paste0("p = ", round(aov$p[row], 2)))
+      subtitle = paste0("Ancova, F(", aov$DFn[row], ", ",
+                        aov$DFd[row], ")", " = ", round(aov$F[row],2),
+                        ", ", p, paste0(", eta^2 = ", round(aov$ges[row], 2)))
+    }
+  }
+  gg1 <- gg1 + ggplot2::labs(subtitle = subtitle, caption = rstatix::get_pwc_label(pwc))
+  return(gg1)
+}
+
+#' ANCOVA Box Plots
+#'
+#' This function create box plots to report results from ANCOVA (AoV).
+#'
+#' @param data a data.frame containing the data in which performing the ANOVA test
+#' @param dv a character string containing the name of variable used as x-axis
+#' @param ivs a vector with character string containing the name of variable used as y-axis
+#' @param aov the ANOVA statistical results returned by rstatix::anova_test
+#' @param pwcs the statistical results returned by a pairwise comparisons between groups from rstatix
+#' @param covar a column name of covariance variable
+#' @param pre.post a column name of time variable (pre- and post-test phases)
+#' @param color a vector or character string containing the name of variable used as color in the box-plot
+#' @param theme the theme used in the box plot
+#' @param step.increase the numeric vector to be used to minimize the overlap
+#' @param subtitle the subtitle in the plot, use number to indicate the row from ANCOVA table
+#' @param ylim the number that indicates the axis-y limit
+oneWayAncovaBoxPlots <- function(
+    data, dv, ivs, aov, pwcs, covar = c(), pre.post = c(), color = c(), theme = c(),
+    step.increase = 0.2, subtitle = c(), ylim = NA) {
+  livs <- as.list(ivs)
+  names(livs) <- ivs
+  return(lapply(livs, FUN = function(iv) {
+    pwc = pwcs
+    if (is.list(pwcs) && is.data.frame(pwcs[[iv]])) pwc = pwcs[[iv]]
+    ggBoxPlotAoC(data, dv, iv, aov, pwc, covar, pre.post, color, theme,
+                 step.increase, subtitle, ylim)
+  }))
+}
+
+
+#' Two-Way ANCOVA Box Plots
+#'
+#' This functions returns a list of Plots related to two-Way ANCOVA test.
+#'
+#' @param data a data.frame containing the data in which performing the ANOVA test
+#' @param dv a column name of dependent variable
+#' @param ivs a list of columns that represents independent variables
+#' @param aov the ANOVA statistical results returned by rstatix::anova_test
+#' @param pwcs the statistical results returned by a pairwise comparisons between groups from rstatix
+#' @param covar a column name of covariance variable
+#' @param pre.post a column name of time variable (pre- and post-test phases)
+#' @param color a vector containing the colors in the box-plot
+#' @param theme the theme used in the bar plot
+#' @param step.increase the numeric vector to be used to minimize the overlap
+#' @param subtitle the subtitle in the plot, use number to indicate the row from ANCOVA table
+#' @param ylim the number that indicates the axis-y limit
+#' @export
+twoWayAncovaBarPlots <- function(
+    data, dv, ivs, aov, pwcs, covar=c(), pre.post = c(), color = c(), theme = c(),
+    step.increase = 0.2, subtitle = c(), ylim = NA) {
+
+  pwc = pwcs
+  if (!is.data.frame(pwcs)) {
+    pwc <- do.call(rbind, lapply(ivs, FUN = function(iv) {
+      pwc = pwcs[[iv]]
+      if (iv == ivs[1]) {
+        pwc$group1 = paste0(pwc$group1, ":" , pwc[[setdiff(ivs, iv)]])
+        pwc$group2 = paste0(pwc$group2, ":" , pwc[[setdiff(ivs, iv)]])
+      } else {
+        pwc$group1 = paste0(pwc[[setdiff(ivs, iv)]], ":", pwc$group1)
+        pwc$group2 = paste0(pwc[[setdiff(ivs, iv)]], ":", pwc$group2)
+      }
+      pwc = pwc[,-c(1)]
+      return(pwc)
+    }))
+  } else {
+    pwc[[paste0(ivs, collapse = ":")]] = apply(pwc[,ivs], 1, paste0, collapse=":")
+  }
+
+  data[[paste0(ivs, collapse = ":")]] = apply(data[,ivs], 1, paste0, collapse=":")
+
+  toReturn = list()
+  toReturn[[paste0(ivs, collapse = ":")]] = ggBoxPlotAoC(
+    data, dv, paste0(ivs, collapse = ":"), aov, pwc, covar, pre.post,
+    color, theme, step.increase, subtitle, ylim)
+  return(toReturn)
+}
